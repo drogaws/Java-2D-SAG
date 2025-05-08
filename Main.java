6d32e0cc5895dadc5c175d11c93d03f4391c8d80
Jadon Browne <163164182+drogaws@users.noreply.github.com>
2025-05-08 11:55:58 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is a test for the GitHub Copilot plugin.");
+        System.out.println("This is a test for the GitHub Copilot plugin. 2nd line.");
     }
-    
 }
